86ab181a1b58653ec7fe2b3e24bb5cb92ef25dd4
ReneeSauce <105013852+ReneeSauce@users.noreply.github.com>
2022-09-16 09:00:22 -0700

    no console.logs in the test

--- tests/index.js
+++ tests/index.js
@@ -22,26 +22,23 @@ test('#addSecret', async (t) => {
 test('should correctly recover secret', async (t) => {
   const expect = []
   t.plan(2 * 255)
-  for (let thresh = 1; thresh < 100; thresh++) {
-    console.log(thresh)
+  for (let thresh = 1; thresh < 256; thresh++) {
     const secret = random(32)
-    console.log(secret)
     const randomPolynomial = createRandomPolynomial(thresh)
     const testPolynom = addSecret(secret, randomPolynomial)
-    //console.log(testPolynom)
     const testPoints = getPoints(testPolynom)
-    //console.log(testPoints)
     const shares = randomPoints(thresh, testPoints)
-    //console.log(shares)
     const recovered = combinePoints(shares)
-    console.log(recovered[0])
-    //console.log(recovered)
-    // todo write tests to ensure recovery is in posible without meeting the threshold
+    // todo write tests to ensure recovery is impossible without meeting the threshold
     // todo: loop test with all or some points
-    //t.equal(testPolynom[0], secret, 'secret and 0 index should be the same')
     t.deepEqual(testPolynom, recovered, 'should be the same')
   }
 })
 
+/*
+test('should work with any pair of points', async(t) => {
+  t.plan(255 * (255))
+})
+*/
 
 
