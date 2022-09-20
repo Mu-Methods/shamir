90fbbdfd8102b8183c321275b2f4b962f62aa442
ReneeSauce <105013852+ReneeSauce@users.noreply.github.com>
2022-09-20 16:45:09 -0700

    reorganized functions. closes #7

--- scripts/shamir.js
+++ scripts/shamir.js
@@ -1,36 +1,42 @@
+const { random, arrayOf } = require('./utilities')
 const { combinePoints } = require('./lagrange.js')
-const crypto = require('crypto')
+const { recoverFrom } = require('./quickLagrange.js')
+const { 
+	createRandomPolynomial,
+	getPoints,
+	randomPoints
+} = require('./polynomial.js')
 
 module.exports = {
-	random,
-	createRandomPolynomial,
-	randomPoints,
 	addSecret,
-	getPoints,
+	createShares,
+	random,
+	arrayOf,
 	combinePoints,
+	recoverFrom,
+	createRandomPolynomial,
+	getPoints,
+	randomPoints
 }
 
 /**
- * Generates a random number
- * @returns float
- * */
-
-function random (num = 32) {
-	return BigInt('0x'+ crypto.randomBytes(num).toString('hex'))
+ * createRandomPolynomial of degree t-1
+ * addSecret to the polynomial
+ * get n points or shares from the polynomial
+ * default number of shares is 255
+ * default x coordinates of n shares are 1 through n 
+ * provide an array for custom selection of x coordinates*/
+function createShares(secret, t, n = 255, array = arrayOf(n)) {
+	shares = []
+	const polynom = createRandomPolynomial(t - 1)
+	polynom.unshift(secret)
+	const points = getPoints(polynom)
+	array.forEach(elem => {
+		shares.push(points[elem])
+	})
+	return shares
 }
 
-/**
- *  generates a degree t polynomial with random coefficients
- * @returns Array<integers>
- * */
-function createRandomPolynomial (t, polynom = []) {
-	if (polynom.length < t - 1) {
-		polynom.push(random())
-		return createRandomPolynomial(t, polynom)
-	}
-
-	return polynom
-}
 
 /**
  * adds the secret to the polynomial
@@ -41,45 +47,8 @@ function addSecret (x, polynom) {
 }
 
 /**
- * returns the y points of the first 256 points
- * */
-function getY (polynom, x) {
-	let y = 0n
-	let i = polynom.length - 1
-	while (i >= 0) {
-		y *= x
-		y += polynom[i]
-		i--
-	}
-	return y
+ * removes the secret from the polynomial*/
+function removeSecret(polynom) {
+	polynom.shift()
+	return polynom
 }
-
-/**
- * returns an array of arrays that have the x index and the y value
- * */
-function getPoints (polynom, points = []) {
-	if (points.length < 256) {
-		const x = BigInt(points.length)
-		points.push(getY(polynom, x))
-		return getPoints(polynom, points)
-	}
-
-	return points
-}
-
-/**
- * returns a threshold of points
- * */
-
-function randomPoints (t, points, array = []) {
-	if (array.length < t) {
-		const num = Math.floor(Math.random() * points.length)
-		p = [BigInt(num), points[num]]
-		if (!array.some(elem => elem[0] === p[0])) {
-			array.push(p)
-		}
-		return randomPoints(t, points, array)
-	}
-	return array
-}
-
--- tests/index.js
+++ tests/index.js
@@ -1,44 +1,102 @@
 var test = require('tape')
 const {
   random,
+  arrayOf,
   addSecret,
   createRandomPolynomial,
   getPoints,
   randomPoints,
-  combinePoints
-} = require('../scripts/shamir')
+  combinePoints,
+  createShares,
+  recoverFrom
+} = require('../scripts/shamir.js')
+
+const { combinate } = require('../scripts/combinatorics.js')
 
 
 test('#addSecret', async (t) => {
   t.plan(1)
   const thresh = 18
-  const secret = random(32)
+  const secret = random()
   const randomPolynomial = createRandomPolynomial(thresh)
   const testPolynom = addSecret(secret, randomPolynomial)
   t.equal(testPolynom[0], secret, 'secret and 0 index should be the same')
 })
 
-
-test('should correctly recover secret', async (t) => {
-  const expect = []
-  t.plan(2 * 255)
+test('should correctly recover polynomial using combinePoints', async (t) => {
+  t.plan(255)
+  const secret = random()
   for (let thresh = 1; thresh < 256; thresh++) {
-    const secret = random(32)
-    const randomPolynomial = createRandomPolynomial(thresh)
+    const randomPolynomial = createRandomPolynomial(thresh - 1)
     const testPolynom = addSecret(secret, randomPolynomial)
     const testPoints = getPoints(testPolynom)
     const shares = randomPoints(thresh, testPoints)
     const recovered = combinePoints(shares)
-    // todo write tests to ensure recovery is impossible without meeting the threshold
-    // todo: loop test with all or some points
-    t.deepEqual(testPolynom, recovered, 'should be the same')
+    t.deepEqual(testPolynom, recovered, 'polynomials should be the same')
   }
 })
 
-/*
-test('should work with any pair of points', async(t) => {
-  t.plan(255 * (255))
+test('should correctly recover secret using recoverFrom', async (t) => {
+  t.plan(255)
+  const secret = random()
+  for (let thresh = 1; thresh < 256; thresh++) {
+    const randomPolynomial = createRandomPolynomial(thresh - 1)
+    const testPolynom = addSecret(secret, randomPolynomial)
+    const testPoints = getPoints(testPolynom)
+    const shares = randomPoints(thresh, testPoints)
+    const recovered = recoverFrom(shares)
+    // todo write tests to ensure recovery is impossible without meeting the threshold
+    // todo: loop test with all or some points
+    t.deepEqual(secret, recovered, 'secret should be the same')
+  }
 })
-*/
 
+
+test('should work with any pair of points', async(t) => {
+  t.plan(32385)
+  const secret = random()
+  const thresh = 2
+  const shares = createShares(secret, thresh)
+
+  const combinations = combinate(arrayOf(255), thresh)
+  combinations.forEach(combo => {
+    const points = []
+    combo.forEach(elem => {
+      points.push(shares[elem])
+    })
+    const recovered = recoverFrom(points)
+    t.equal(secret, recovered, 'secrets should be the same')    
+  })
+})
+
+test('should work with any 3 points', async(t) => {
+  t.plan(2731135)
+  const secret = random()
+  const thresh = 3
+  const shares = createShares(secret, thresh)
+
+  const combinations = combinate(arrayOf(255), thresh)
+  combinations.forEach(combo => {
+    const points = []
+    combo.forEach(elem => {
+      points.push(shares[elem])
+    })
+    const recovered = recoverFrom(points)
+    t.equal(secret, recovered, 'secrets should be the same')    
+  })
+})
+
+test('can\'t recover secret without enough points', async (t) => {
+  t.plan(254)
+  const secret = random()
+  for (let thresh = 2; thresh < 256; thresh++) {
+    const randomPolynomial = createRandomPolynomial(thresh - 1)
+    const testPolynom = addSecret(secret, randomPolynomial)
+    const testPoints = getPoints(testPolynom)
+    testPoints.shift()
+    const shares = randomPoints(thresh - 1, testPoints)
+    const recovered = recoverFrom(shares)
+    t.equal(true, recovered !== secret, 'secrets should be different')
+  }
+})
 
