0659195df48fbe2c1ea5fc4e214624f16edcbabe
frankiebee <frankie.diamond@gmail.com>
2022-11-10 12:01:16 -1000

    setup ts and remove packag-lock for yarn

--- scripts/shamir-ts/index.ts
+++ scripts/shamir-ts/index.ts
@@ -15,7 +15,7 @@ module.exports = {
  * @returns float
  * */
 
-function random (num = 32) {
+function random (num: number = 32): bigint {
 	return BigInt('0x'+ crypto.randomBytes(num).toString('hex'))
 }
 
@@ -23,7 +23,7 @@ function random (num = 32) {
  *  generates a degree t polynomial with random coefficients
  * @returns Array<integers>
  * */
-function createRandomPolynomial (t, polynom = []) {
+function createRandomPolynomial (t: number, polynom: Array<bigint> = []): Array<bigint> {
 	if (polynom.length < t - 1) {
 		polynom.push(random())
 		return createRandomPolynomial(t, polynom)
@@ -35,7 +35,7 @@ function createRandomPolynomial (t, polynom = []) {
 /**
  * adds the secret to the polynomial
  * */
-function addSecret (x, polynom) {
+function addSecret (x: bigint, polynom: Array<bigint>): Array<bigint> {
 	polynom.unshift(x)
 	return polynom
 }
@@ -43,7 +43,7 @@ function addSecret (x, polynom) {
 /**
  * returns the y points of the first 256 points
  * */
-function getY (polynom, x) {
+function getY (polynom:Array<bigint>, x:bigint): bigint {
 	let y = 0n
 	let i = polynom.length - 1
 	while (i >= 0) {
@@ -57,25 +57,32 @@ function getY (polynom, x) {
 /**
  * returns an array of arrays that have the x index and the y value
  * */
-function getPoints (polynom, points = []) {
-	if (points.length < 256) {
-		const x = BigInt(points.length)
-		points.push(getY(polynom, x))
-		return getPoints(polynom, points)
+
+function getPoints (polynom:Array<bigint>, points:Array<bigint>= []): Array<bigint> {
+	const pointsCopy = [...points]
+	if (pointsCopy.length < 256) {
+		const x = BigInt(pointsCopy.length)
+		pointsCopy.push(getY(polynom, x))
+		return getPoints(polynom, pointsCopy)
 	}
 
-	return points
+	return pointsCopy
 }
 
 /**
  * returns a threshold of points
  * */
 
-function randomPoints (t, points, array = []) {
+type tPoints = [bigint, bigint]
+
+function randomPoints (t: number, points: Array<bigint>, array:Array<tPoints> = []): Array<tPoints> {
+	if (points.length === 0) throw new Error('points array is empty')
 	if (array.length < t) {
 		const num = Math.floor(Math.random() * points.length)
-		p = [BigInt(num), points[num]]
+		const p = [BigInt(num), points[num]]
 		if (!array.some(elem => elem[0] === p[0])) {
+		// TODO: try to remove ts ignore line bellow and fix error
+		//@ts-ignore: next line
 			array.push(p)
 		}
 		return randomPoints(t, points, array)
